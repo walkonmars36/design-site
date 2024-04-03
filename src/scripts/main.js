028c80ee90cd3a1840454d1998d4aebc4d2522f1
Mark Lawson <mark@walkonmars.co.uk>
2024-04-03 13:12:09 +0100

    enabled netlify forms

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,1 @@
 // Link Underline
-
-const links = document.querySelectorAll("a");
-links.forEach(function (link) {
-  link.addEventListener("touchstart", function () {
-    this.classList.add("active");
-  });
-  link.addEventListener("touchend", function () {
-    this.classList.remove("active");
-  });
-  link.addEventListener("mouseenter", function () {
-    this.classList.add("hover");
-  });
-  link.addEventListener("mouseleave", function () {
-    this.classList.remove("hover");
-  });
-});
