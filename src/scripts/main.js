770a11315e9fadcd00cd3d2e517bfba62868f5f7
Mark Lawson <mark@walkonmars.co.uk>
2024-04-03 15:34:00 +0100

    moved read more show less logic to main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
-// Link Underline
+// read more button
+const readMore = document.querySelectorAll(".read-more");
+const showLess = document.querySelectorAll(".show-less");
+const moreText = document.querySelectorAll(".more-text");
+
+readMore.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    moreText[i].classList.add("visible");
+    readMore[i].style.display = "none";
+    showLess[i].style.display = "block";
+  });
+});
+
+showLess.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    moreText[i].classList.remove("visible");
+    readMore[i].style.display = "block";
+    showLess[i].style.display = "none";
+  });
+});
