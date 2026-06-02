4b9df75acd62ade5e45b49bcd534b807ef6f1470
Mark Lawson <mark@walkonmars.co.uk>
2026-06-02 13:29:36 +0100

    Change to output static.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,21 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 
-import node from "@astrojs/node";
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [
     icon(),
     sanity({
       projectId: "azfpwdmk",
       dataset: "production",
-      useCdn: false,
+      useCdn: true,
       studioBasePath: "/studio",
     }),
     react(),
   ],
 
-  adapter: node({
-    mode: "standalone",
-  }),
-
-  output: "server",
+  output: "static",
 
   vite: {
     optimizeDeps: {
